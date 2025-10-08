25f8ab1a2a572c74702e4b6e4818bdab8fdf3fad
AnnaReachmack <annareachmack@vt.edu>
2025-10-08 15:48:29 -0400

    my mess

---
+++ classmaterials/annaweek4classscript.R
@@ -0,0 +1,13 @@
+#in class assignment
+#getwd() see what youre directory you're in
+#dir() see what's in the directory
+#change direction: session>change>choose directory path
+
+alphabet <- c("a","b", "c")
+alphabet
+# the <- will assign a name on the left to whatever values are on the right
+
+summedbirthnumbers <- 4+4+1998
+summedbirthnumbers
+1+1
+1dsfsd
---
+++ classmaterials/assignment3.R
@@ -0,0 +1,9 @@
+
+#script contains raw data from my fieldwork in Fire Island summer 2025
+
+
+#reading in data
+RawData=read.csv("C:/Users/annar/OneDrive/Documents/FIIS/2025fielddata.csv")
+
+#using this package for figures
+library(ggplot2)
